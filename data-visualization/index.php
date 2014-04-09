80e7323af547b7e6c29c10ee2938e009dc16be41
Luiz Mendez <homemadestudios@gmail.com>
2014-04-09 00:51:18 -0500

    Add Data Visualizer

---
+++ data-visualization/index.php
@@ -0,0 +1,76 @@
+<?php  
+	/*
+		Error Reporting
+	*/
+	ini_set('display_errors',1);
+	error_reporting(-1);
+	
+	/*
+		Get the url contents and use json_decode to parse it as an array
+	*/
+	$url = 'http://api.worldbank.org/countries/USA/indicators/3.1_RE.CONSUMPTION?per_page=50&date=2000:2014&format=json';
+	$content = file_get_contents($url);
+	$json = json_decode($content, true);
+	$array = array_reverse($json[1]); /* The array which contain the useful data is reversed and saved for easy calling */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+  	<title>Data Visualization Test</title>
+  	
+  	<style>
+  		#chart {
+  			margin: 0 auto;
+  		}
+  	</style>
+  	
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Energy Consumption', { role: 'style' }],
+          <?php  
+          	/*
+          		Parse the array to show the year and energy consuption in the table.
+          		The energy consuption is rounded to millions for easy reading.
+          		Each bar is given a color depending on the value of energy consuption. 
+          	*/
+          	foreach ($array as $item) {
+          		if ($item['value']) {
+          			$year = $item['date'];
+          			$value = round($item['value']/1000000,2);
+          			$color = 'GreenYellow';
+          			if($value>2.9 && $value<=3.6){
+          				$color = 'gold';
+          			} else if ($value>3.6 && $value<=4.3) {
+						$color = 'orange';
+					} else if ($value>4.3 && $value<=5.0) {
+						$color = 'red';
+					}
+          			
+          			echo "['".$year."',".$value.",'".$color."'],";
+          		}
+          	}
+          ?>
+        ]);
+
+        var options = {
+          title: 'Energy Consumption in the US in Millions',
+          hAxis: {title: 'Year'},
+          legend: { position: "none" }
+        };
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
+        chart.draw(data, options);
+      }
+    </script>
+    
+  </head>
+  <body>
+    <div id="chart" style="width: 960px; height: 600px;"></div>
+  </body>
+</html>
--- fizzbuzz/index.php
+++ fizzbuzz/index.php
@@ -1,5 +1,8 @@
 <?php
 	
+	/*
+		A loop from 1 to 100 and if the modulus of the number is 0 when divided by 3, 5 or both it prints Fizz, Buzz or FizzBuzz! respectively 
+	*/
 	function fizzbuzz(){
 		for ($i = 1; $i < 101; $i++) {
 			if(($i%3==0) && ($i%5==0)){
@@ -15,6 +18,9 @@
 		}
 	}
 	
+	/*
+		Function to test if a number is Fizz, Buzz, FizzBuzz! or none
+	*/
 	function test_fizzbuzz($n=1) {
 		if(($n%3==0) && ($n%5==0)){
 			print ($n." is divisible by 3 and 5 so FizzBuzz!");
@@ -27,7 +33,7 @@
 		}
 	}
 	
-	/*test_fizzbuzz();*/
+	/*test_fizzbuzz(15);*/
 	fizzbuzz();
 
 ?>
